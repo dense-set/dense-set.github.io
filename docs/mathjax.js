78f1cd36aa62ee6284eddbaa111c505f7bd704bf
dense-set <vijaysarthak23@gmail.com>
2024-04-01 22:02:48 +0530

    First blogpost!

---
+++ docs/mathjax.js
@@ -0,0 +1,16 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$']],
+
+    macros: {
+      RR: "{\\bf R}",
+      bold: ["{\\bf #1}", 1]
+    },
+
+    environments: {}
+  },
+
+  svg: {
+    fontCache: 'global'
+  }
+};

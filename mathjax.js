3fa42867880f4bd164f62c29c6118054c33a5af6
dense-set <vijaysarthak23@gmail.com>
2024-04-02 19:13:00 +0530

    Made math overflow scrollable

--- mathjax.js
+++ mathjax.js
@@ -1,11 +1,11 @@
-MathJax = {
-  tex: {
-    inlineMath: [
-      ['\\(', '\\)']
-    ],
-    displayMath: [
-      ['$$', '$$'],
-      ['\\[', '\\]']
-    ]
-  }
-};
+document.addEventListener("DOMContentLoaded", function() {
+  var mathElements = document.querySelectorAll('.math.display');
+  mathElements.forEach(function(element) {
+      var wrapperDiv = document.createElement('div');
+      wrapperDiv.style.overflowX = 'auto';
+      while (element.firstChild) {
+          wrapperDiv.appendChild(element.firstChild);
+      }
+      element.appendChild(wrapperDiv);
+  });
+});

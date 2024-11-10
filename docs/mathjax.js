7d1ba536a1e262aaedbfa25c9d4d146240433872
dense-set <vijaysarthak23@gmail.com>
2024-11-10 05:34:09 +0530

    For PhD

--- docs/mathjax.js
+++ docs/mathjax.js
@@ -1,16 +1,11 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$']],
-
-    macros: {
-      RR: "{\\bf R}",
-      bold: ["{\\bf #1}", 1]
-    },
-
-    environments: {}
-  },
-
-  svg: {
-    fontCache: 'global'
-  }
-};
+document.addEventListener("DOMContentLoaded", function() {
+  var mathElements = document.querySelectorAll('.math.display');
+  mathElements.forEach(function(element) {
+      var wrapperDiv = document.createElement('div');
+      wrapperDiv.style.overflowX = 'auto';
+      while (element.firstChild) {
+          wrapperDiv.appendChild(element.firstChild);
+      }
+      element.appendChild(wrapperDiv);
+  });
+});
